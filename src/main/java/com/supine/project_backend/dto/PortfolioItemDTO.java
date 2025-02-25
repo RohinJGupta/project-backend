1ecf75fc4c4f8295cd093a0652ddc70b7e551f8b
Rohin Gupta <rohinjgupta@gmail.com>
2025-02-25 15:17:44 -0500

    created dto

---
+++ src/main/java/com/supine/project_backend/dto/PortfolioItemDTO.java
@@ -0,0 +1,77 @@
+package com.supine.project_backend.dto;
+
+import java.time.Instant;
+import java.time.LocalDate;
+
+public class PortfolioItemDTO {
+
+    private String title;
+    private String description;
+    private float projectCost;
+    private int duration;
+    private String category;
+    private boolean isFeatured;
+    private LocalDate projectDate;
+
+
+    // Getters and Setters
+
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getProjectCost() {
+        return projectCost;
+    }
+
+    public void setProjectCost(float projectCost) {
+        this.projectCost = projectCost;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public boolean isFeatured() {
+        return isFeatured;
+    }
+
+    public void setIsFeatured(boolean isFeatured) {
+        this.isFeatured = isFeatured;
+    }
+
+    public LocalDate getProjectDate() {
+        return projectDate;
+    }
+
+    public void setProjectDate(LocalDate projectDate) {
+        this.projectDate = projectDate;
+    }
+
+} 
---
+++ src/main/java/com/supine/project_backend/dto/ServiceProviderDTO.java
@@ -0,0 +1,82 @@
+package com.supine.project_backend.dto;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+
+import org.locationtech.jts.geom.Point;
+
+public class ServiceProviderDTO {
+
+
+
+    private String businessName;
+    private String biography;
+    private String certifications;
+    private boolean backgroundCheck;
+    private Point location;
+    private String availability;
+    private LocalDateTime lastActive;
+
+
+    // Getters and Setters
+
+
+
+    public String getBusinessName() {
+        return businessName;
+    }
+
+    public void setBusinessName(String businessName) {
+        this.businessName = businessName;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+
+    public String getCertifications() {
+        return certifications;
+    }
+
+    public void setCertifications(String certifications) {
+        this.certifications = certifications;
+    }
+
+    public boolean isBackgroundCheck() {
+        return backgroundCheck;
+    }
+
+    public void setBackgroundCheck(boolean backgroundCheck) {
+        this.backgroundCheck = backgroundCheck;
+    }
+
+    public Point getLocation() {
+        return location;
+    }
+
+    public void setLocation(Point location) {
+        this.location = location;
+    }
+
+    public String getAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(String availability) {
+        this.availability = availability;
+    }
+
+    public LocalDateTime getLastActive() {
+        return lastActive;
+    }
+
+    public void setLastActive(LocalDateTime lastActive) {
+        this.lastActive = lastActive;
+    }
+
+
+} 
