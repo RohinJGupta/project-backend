e68df2e914955081d47203b35c184c3fde46d160
Rohin Gupta <rohinjgupta@gmail.com>
2025-03-07 14:42:20 -0500

    user attributes

--- src/main/java/com/supine/project_backend/model/User.java
+++ src/main/java/com/supine/project_backend/model/User.java
@@ -25,6 +25,10 @@ public class User implements UserDetails {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String email;
+    private String passwordHash;
+    private String phone;
+
     @Enumerated(EnumType.STRING)
     private UserRole userRole;
 
@@ -49,92 +53,5 @@ public class User implements UserDetails {
     @JoinColumn(name = "vendor_id", referencedColumnName = "id")
     private Vendor vendor;
     
-
-
-    //getters
-
-    public Long getId() {
-        return id;
-    }
-    
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public Instant getCreatedAt() {
-        return createdAt;
-    }   
-
-    public Instant getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public boolean isProvider() {
-        return isProvider;
-    }   
-
-    public Vendor getVendor() {
-        return vendor;
-    }
-
-    public Point getLocation() {
-        return location;
-    }
-
-    public boolean getHasOnboarded() {
-        return hasOboarded;
-    }
-
-
-
-
- 
-
-    //setters
-
-
-
-    public void setId(Long id) {
-        this.id = id;
-    }         
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }   
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }   
-
-    public void setCreatedAt(Instant createdAt) {
-        this.createdAt = createdAt;
-    }    
-
-    public void setUpdatedAt(Instant updatedAt) {
-        this.updatedAt = updatedAt;
-    }
- 
-
-    public void setIsProvider(boolean isProvider) {
-        this.isProvider = isProvider;
-    }   
-
-    public void setVendor(Vendor vendor) {
-        this.vendor = vendor;
-    }   
-
-    public void setLocation(Point location) {
-        this.location = location;
-    }
-
-    public void setHasOnboarded(boolean hasOboarded) {
-        this.hasOboarded = hasOboarded;
-    }
- 
 
 }
